27899bb65374714106d582fc8d4fed767f37b013
vlad980000 <vlad980000@mail.ru>
2022-03-23 22:13:37 +0300

    Comit

--- Assets/Scripts/RespawnEnemy.cs
+++ Assets/Scripts/RespawnEnemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RespawnEnemy : MonoBehaviour
 {
-    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Enemy _enemyPrefab;
     [SerializeField] private Transform[] _spawnPoints;
     [SerializeField] private float _secondsBetvenSpawn;
 
