6c58490584bc15e764b6928133cdb76ba7c33c1c
vlad980000 <vlad980000@mail.ru>
2022-03-23 22:03:52 +0300

    init

---
+++ Assets/Scripts/MovementEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RespawnEnemy))]
+public class MovementEnemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position = new Vector3(transform.position.x + _speed * Time.deltaTime, transform.position.y);
+    }
+}
---
+++ Assets/Scripts/RespawnEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _secondsBetvenSpawn;
+
+    private float _elapserTime = 0;
+
+    private void Update()
+    {
+        _elapserTime += Time.deltaTime;
+
+        if(_elapserTime >= _secondsBetvenSpawn)
+        {
+            _elapserTime = 0;
+            int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+            Instantiate(_enemyPrefab, _spawnPoints[spawnPointNumber]);
+        }
+    }
+}
